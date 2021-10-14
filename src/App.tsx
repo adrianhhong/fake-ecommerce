fd743f141e5fe3dbe7936c627c7e9190193c22e3
Adrian Hong <Adrian.Hong@team.telstra.com>
2021-10-14 17:47:14 +1100

    :wrench: Changed router to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Main from "./views/Main";
 import Profile from "./views/Profile";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
